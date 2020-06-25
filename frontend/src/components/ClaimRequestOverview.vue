001a15f70ef1706f56d318ec02a802f835fd9ad3
philippcl <PhilippCl@users.noreply.github.com>
2020-06-25 12:44:17 +0200

    Finished ClaimRequestUI

--- frontend/src/components/ClaimRequestOverview.vue
+++ frontend/src/components/ClaimRequestOverview.vue
@@ -1,14 +1,42 @@
 <template>
 <v-card>
-    <v-toolbar
-        color="primary"
-        dark
-      >
-      <v-toolbar-title>Bisherige Meldungen und Anträge</v-toolbar-title>
-    </v-toolbar>
-    feefefe
+    <v-card-title>
+      Bisherige Meldungen und Anträge
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="dataset"
+      :search="search"
+    ></v-data-table>
 </v-card>
 
 </template>
 
-<script></script>
+<script>
+  export default {
+    data () {
+      return {
+        search: '',
+        headers : [
+          {
+            text: 'Antragsteller',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Beschreibung', value: 'description' },
+          { text: 'Typ', value: 'type' },
+          { text: 'Status', value: 'state' },
+        ],
+      }
+    },
+  }
+</script>
