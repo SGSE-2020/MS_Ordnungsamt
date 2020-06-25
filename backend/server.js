f80180ffe2660a44cd66b1b1f07656c77740bc4a
philippcl <PhilippCl@users.noreply.github.com>
2020-06-25 19:17:24 +0200

    Added GRPC Client to Server

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,6 @@
-var express = require('express')
-var app = express()
-var port = process.env.PORT || 8080;
-var gn_count = 0
-var ow_count = 0
 
-var server = app.listen(port, function() {
-  console.log('MS_Ordnungsamt is running! Port:' + port);
-});
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+require('./services/rest_service')
+require('./services/grpc_service')
 
-app.get('/ordnungswidrigkeiten', function (req, res) {
-  res.json({ow : [ "ordnungswidrigkeiten"+gn_count++ , "ordnungswidrigkeiten"+gn_count++ , "ordnungswidrigkeiten"+gn_count++ , "ordnungswidrigkeiten"+gn_count++]});
-});
-
-app.get('/genehmigungen', function (req, res) {
-  res.json({gn : [ "genehmigungen"+ow_count++ , "genehmigungen"+ow_count++ , "genehmigungen"+ow_count++ , "genehmigungen"+ow_count++]});
-});
+//TODO Database Stuff
---
+++ backend/services/rest_service.js
@@ -0,0 +1,52 @@
+var express = require('express')
+const caller = require('grpc-caller')
+const path = require('path');
+var app = express()
+
+var port = process.env.PORT || 8080;
+
+//Proto Files
+const PROTO_PATH_ANNOUNCEMENT = path.resolve(__dirname, '../proto/announcement.proto')
+const PROTO_PATH_USER = path.resolve(__dirname, '../proto/user.proto')
+
+
+//Tempo
+var gn_count = 0
+var ow_count = 0
+
+/*Header*/
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+});
+    
+/**
+* Returns if the rest server is alive
+*/
+app.get("/alive", function(req, res) {
+    console.log("REST CALL: /alive");
+    let responseObj = {
+        status:"success",
+        message: "Server is alive."
+    }
+    res.status(200);
+    res.send(responseObj);
+});
+
+app.get('/ordnungswidrigkeiten', function (req, res) {
+    console.log("REST CALL: /ordnungswidrigkeiten");
+    res.json({ow : [ "ordnungswidrigkeiten"+gn_count++ , "ordnungswidrigkeiten"+gn_count++ , "ordnungswidrigkeiten"+gn_count++ , "ordnungswidrigkeiten"+gn_count++]});
+});
+
+app.get('/genehmigungen', function (req, res) {
+    console.log("REST CALL: /genehmigungen");
+    res.json({gn : [ "genehmigungen"+ow_count++ , "genehmigungen"+ow_count++ , "genehmigungen"+ow_count++ , "genehmigungen"+ow_count++]});
+});
+
+
+//Server start
+var server = app.listen(port, function() {
+    console.log('MS_Ordnungsamt is running! Port:' + port);
+});
