ea8262d488bf337c189a7ea44d5a75413f47ada9
philippcl <PhilippCl@users.noreply.github.com>
2020-06-30 18:20:06 +0200

    Fehlersuche 010 Jetzt mit Hilfe

--- backend/services/amqp_service.js
+++ backend/services/amqp_service.js
@@ -1,10 +1,20 @@
 
 var amqp = require('amqp');
 
+const connectionObj = {
+  host: 'ms-rabbitmq',
+  port: 5672,
+  login: 'testmanager',
+  password: 'sgseistgeil',
+  vhost: '/'
+};
+
 var amqp_url = "amqp://administrator:sgse2020@ms-rabbitmq:5672/";
 var amqp_url_dev = "amqp://aelsovie:l-gjNtM4_dqaFQdB-ZAOTrGZljQmAGWq@wolverine.rmq.cloudamqp.com/aelsovie";
 
-var connection = amqp.createConnection({ url: amqp_url});
+//var connection = amqp.createConnection({ url: amqp_url});
+const connection = amqp.createConnection(connectionObj);
+
 var exc = null;
 var last_error = null
 
@@ -17,9 +27,19 @@ connection.on('error', function(e) {
 connection.on('ready', function () {
   console.log('[AMQP-Service] Connection init complete.')
   last_error = "Connection init complete"
-  exc = connection.exchange('ordnungsamt2',{type : 'fanout'}, function (exchange) {
-    console.log('Exchange ' + exc.name + ' is open');
-    last_error = "Exchange is open";
+  //exc = connection.exchange('ordnungsamt2',{type : 'fanout'}, function (exchange) {
+    //console.log('Exchange ' + exc.name + ' is open');
+    //last_error = "Exchange is open";
+  //});
+  exc = connection.exchange('ordungsamt', {
+    type: 'FANOUT',
+    durable: true,
+    autoDelete: false
+  }, (exchangeRes) => {
+    last_error = "AMQP exchange '" + exchangeRes.name + "' established.";
+  });
+  exc.on('error', error => {
+    last_error = "AMQP Exchange error: " + error.message;
   });
 });
 
