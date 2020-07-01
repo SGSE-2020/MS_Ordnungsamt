50219779e6f0ea156d9e4df2f3056785b1369553
philippcl <PhilippCl@users.noreply.github.com>
2020-07-01 09:40:49 +0200

    Bind to parkplatz exchange

--- backend/services/amqp_service.js
+++ backend/services/amqp_service.js
@@ -9,37 +9,43 @@ const connectionObj = {
   vhost: '/'
 };
 
-var amqp_url = "amqp://administrator:sgse2020@ms-rabbitmq:5672/";
-var amqp_url_dev = "amqp://aelsovie:l-gjNtM4_dqaFQdB-ZAOTrGZljQmAGWq@wolverine.rmq.cloudamqp.com/aelsovie";
+const exchange_parkplatz = 'parkplatz';
+const exchange_ordnungsamt = 'ordnungsamt';
+const queue_ordnungsamt_parkplatz = 'ordnungsamt_falschparker';
 
-//var connection = amqp.createConnection({ url: amqp_url});
 const connection = amqp.createConnection(connectionObj);
 
 var exc = null;
-var last_error = null
+var amqp_log = []
 
 connection.on('error', function(e) {
   console.log("[AMQP-Service] ", e);
-  last_error = "AMQP Connection error: " + e;
+  amqp_log.push("AMQP Connection error: " + e);
 });
-   
 
 connection.on('ready', function () {
   console.log('[AMQP-Service] Connection init complete.')
-  last_error = "Connection init complete"
-  //exc = connection.exchange('ordnungsamt2',{type : 'fanout'}, function (exchange) {
-    //console.log('Exchange ' + exc.name + ' is open');
-    //last_error = "Exchange is open";
-  //});
-  exc = connection.exchange('ordnungsamt', {
+  amqp_log.push("Connection init complete");
+  //Define exchange to publish
+  exc = connection.exchange(exchange_ordnungsamt, {
     type: 'fanout',
     durable: true,
     autoDelete: false
   }, (exchangeRes) => {
-    last_error = "AMQP exchange '" + exchangeRes.name + "' established.";
+    amqp_log.push("AMQP exchange '" + exchangeRes.name + "' established.");
   });
   exc.on('error', error => {
-    last_error = "AMQP Exchange error: " + error.message;
+    amqp_log.push("AMQP Exchange error: " + error.message);
+  });
+  //Define queue to bind
+  connection.queue(queue_ordnungsamt_parkplatz, queue => {
+    amqp_log.push("Queue" + queue_ordnungsamt_parkplatz + "created.");
+    queue.bind(exchange_parkplatz, queue_ordnungsamt_parkplatz, callback => {
+      amqp_log.push("AMQP queue '" + queue.name + "' is bound to exchange: " + exchange_parkplatz + ".");
+      queue.subscribe((msg) => {
+        amqp_log.push("Message incoming: " + msg);
+      });
+    });
   });
 });
 
@@ -50,6 +56,6 @@ module.exports  = {
     exc.publish('','Testmessageblblblb');
   },
   lastError : () => {
-    return last_error;
+    return amqp_log;
   }
 };
