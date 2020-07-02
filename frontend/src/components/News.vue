efcf57b1be59d366e01fc2dc005bedb76e12a178
philippcl <PhilippCl@users.noreply.github.com>
2020-07-02 13:30:20 +0200

    Removed lorem ipsum

--- frontend/src/components/News.vue
+++ frontend/src/components/News.vue
@@ -4,13 +4,7 @@
         Ordnungsamt Neuigkeiten
     </v-card-title>
     <v-card-text>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras euismod iaculis leo, at mattis nisi vulputate vel. In semper lectus nunc, vel tristique libero sodales non. Donec mollis magna ac massa ultrices, sed sagittis enim sollicitudin. Curabitur malesuada sapien a tellus iaculis placerat. Nullam lobortis varius facilisis. Mauris aliquam ultricies interdum. Nam quis ipsum magna.
-
-Aenean faucibus leo eget velit tincidunt, sed accumsan leo vehicula. Donec ipsum lectus, aliquam non eleifend quis, tincidunt a arcu. Mauris dapibus urna non tellus commodo hendrerit. Praesent dignissim lectus sit amet auctor aliquet. Fusce dolor tortor, consectetur ac convallis sed, tempus vel nisl. Curabitur sit amet est quis nibh convallis commodo ut eu nulla. Donec quis quam quis erat malesuada fermentum at eget leo. Vivamus et nulla sed purus blandit condimentum. Quisque vitae risus convallis, varius ante quis, posuere arcu. Nullam vitae risus molestie, faucibus dui vitae, elementum quam. Praesent vitae tortor in tortor pellentesque volutpat ac in arcu.
-
-Nunc et quam maximus, consectetur tellus ac, pulvinar lectus. Nulla laoreet felis vel imperdiet suscipit. Maecenas et massa ut felis hendrerit bibendum ac ac orci. Duis venenatis ipsum ac risus tincidunt tincidunt. Morbi sit amet ultrices justo, eget congue mauris. Maecenas vitae lectus ac nulla gravida ornare. Nunc nec erat malesuada, consectetur odio a, porta ipsum. Nullam sed semper nibh, non porta dolor. Nulla facilisi. Maecenas nec tincidunt velit. Aliquam at quam vel ante rutrum gravida ut vitae ipsum.
-
-Vestibulum nec massa dictum, tempor lacus a, condimentum leo. In mi elit, condimentum nec tempus aliquet, consectetur eu mauris. Mauris interdum pellentesque sem, a pharetra nisl porta eget. Nunc dignissim magna interdum, sodales velit a, congue nibh. Quisque feugiat magna nec ipsum suscipit, a aliquam sem rhoncus. Duis ultrices purus vel ipsum iaculis, vel tincidunt ex rhoncus. Praesent sit amet erat id odio eleifend tempor eu eget augue. Nullam id efficitur dolor. Nullam hendrerit mi est, vel condimentum metus mollis non. Cras commodo facilisis ligula et consectetur. Sed mollis magna in ultricies hendrerit. Aenean nec nulla in est mattis eleifend. Mauris mollis purus ac augue aliquet, scelerisque euismod libero gravida.
+        Aktuell keine News, auch mal schön :)    
     </v-card-text>
 </v-card>
 </template>
